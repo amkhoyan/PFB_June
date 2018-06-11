287201b1aba4c62f0b43bfe58f84e3ad3a869241
amkhoyan <mkhoyan12@gmail.com>
2018-06-11 21:02:49 +0400

    code for 'operators' lecture

---
+++ Operators/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int res1 = 16 % 3;
+            //Console.WriteLine(res1);
+
+            //bool res2 = 6 > 5;
+
+            //int x = 10;
+            //int y = 15;
+
+            //bool b1 = x == y;
+            //bool b2 = x != y;
+
+            //Console.WriteLine($"x == y: {b1}, x != y: {b2}");
+            //Console.WriteLine("x == y: {0}, x != y: {1}", b1, b2);
+
+            //bool b3 = x > 15 && y < 20;
+            //bool b4 = x > 8 || y < 10;
+
+            //Console.WriteLine($"b3:{b3}, b4:{b4}");
+
+            //int x1 = 20;
+            //int x2 = x1++;
+            //Console.WriteLine(x1);
+            //int x3 = ++x1;
+            //Console.WriteLine(x1);
+
+            //Console.WriteLine($"x2:{x2}, x3:{x3}");
+
+            //bool b5 = true;
+            //Console.WriteLine($"b5: {b5}, b5 rev: {!b5}");
+            //bool b6 = !b5;
+            //Console.WriteLine(b6);
+
+
+            //bool res = x1 > 456 & ++x2 > 15 & x3 > 6 & b5 == true & b6 == false;
+
+            //bool res2 = x1 < 456 | ++x2 < 15 | x3 < 6 | b5 != true | b6 != false;
+
+
+
+            //int myInt = 2 ^ 3;
+            //Console.WriteLine($"& {2 & 3 }, | {2 | 3 }, ^ {2 ^ 3 ^ 4 }");
+
+            //int myInt2 = 2 >> 3;
+
+            Console.WriteLine($"2 >> 1: {2 >> 1}");
+
+
+            //string input1 = Console.ReadLine();
+            //string input2 = Console.ReadLine();
+
+
+            //int result = input1 == input2 ? 1 : 0;
+
+            //Console.WriteLine(result);
+
+            //Console.Write("Please enter your input Number: ");
+            //int inp_num = int.Parse(Console.ReadLine());
+
+            //string result = inp_num % 2 == 0 ? "even" :"odd";
+
+            //Console.WriteLine(result);
+
+
+            //int first_num = inp_num / 10;
+            //int second_num = inp_num % 10;
+
+            //Console.WriteLine($"  first_num is  :{first_num} \r\n second_num is : {second_num}");
+
+            Console.ReadLine();
+        }
+    }
+}
